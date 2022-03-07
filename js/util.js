2904e3910591217a1864166bd15ace4498da6675
rseusebio <rafaelsantos.rse@gmail.com>
2022-03-07 15:04:29 -0300

    first commit

---
+++ js/util.js
@@ -0,0 +1,20 @@
+function ajax(url, method, callback) {
+  let request = new XMLHttpRequest();
+  request.overrideMimeType("application/json");
+  request.open(method, url, true);
+  request.onreadystatechange = () => {
+    if (request.readyState === 4 && request.status == "200") {
+      callback(request.responseText);
+    }
+  };
+  request.send(null);
+}
+
+
+function vote(vereadorId, prefeitoId) {
+  ajax(`3.83.161.213:8080/vote/${vereadorId}/${prefeitoId}`,
+    "POST",
+    (res => {
+      console.log("response", res);
+    }));
+}
